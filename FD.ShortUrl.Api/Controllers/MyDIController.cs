715b9d40c7f33414a07767da4841b9b83e1432c3
dragon.ice.995 <dragon.ice.995@hotmail.com>
2022-02-21 17:51:04 +0800

    -l mydi

---
+++ FD.ShortUrl.Api/Controllers/MyDIController.cs
@@ -0,0 +1,25 @@
+﻿using FD.ShortUrl.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FD.ShortUrl.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyDIController : ControllerBase
+    {
+        private readonly IMyDependency _myDependency;
+
+        public MyDIController(IMyDependency myDependency)
+        {
+            _myDependency = myDependency;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _myDependency.WriteMessage("myDependenncy");
+            return Ok("myDependenncy");
+        }         
+    }
+}
--- FD.ShortUrl.Api/Program.cs
+++ FD.ShortUrl.Api/Program.cs
@@ -1,31 +1,7 @@
-
 using FD.ShortUrl.Api;
-using FD.ShortUrl.Repository;
-using Microsoft.AspNetCore.Http.Json;
-using Microsoft.EntityFrameworkCore;
-
-
+using FD.ShortUrl.Core;
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationDbContext>(opt =>
-    opt.UseOracle(configuration.GetConnectionString("baseDb")));
-// Configure JSON options.
-builder.Services.Configure<JsonOptions>(options =>
-{
-    options.SerializerOptions.PropertyNamingPolicy = new UpperCaseNamingPolicy();
-    options.SerializerOptions.WriteIndented = true;
-});
-builder.Services.AddMvcCore().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.PropertyNamingPolicy = new UpperCaseNamingPolicy();
-    options.JsonSerializerOptions.WriteIndented = true;
-});
-
+builder.Services.AddHostedService<NormalHostedService>();
 var app = builder.Build();
-app.UseDefaultFiles();
-app.UseStaticFiles();
-app.MapControllers();
- 
-app.Run();
 
+app.Run();
