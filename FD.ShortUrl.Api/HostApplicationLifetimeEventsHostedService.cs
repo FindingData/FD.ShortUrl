e1b24902af51aee10f4758d24d8f8c69ec7e77f2
dragon.ice.995 <dragon.ice.995@hotmail.com>
2022-03-02 18:46:58 +0800

    fix

---
+++ FD.ShortUrl.Api/HostApplicationLifetimeEventsHostedService.cs
@@ -0,0 +1,51 @@
+﻿namespace FD.ShortUrl.Api
+{
+    public class HostApplicationLifetimeEventsHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ILogger _logger;
+        protected readonly IConfiguration _configuration;
+
+        public HostApplicationLifetimeEventsHostedService(
+            IHostApplicationLifetime hostApplicationLifetime,
+            ILogger<HostApplicationLifetimeEventsHostedService> logger, 
+            IConfiguration configuration)
+        {
+
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _hostApplicationLifetime.ApplicationStarted.Register(OnStarted);
+            _hostApplicationLifetime.ApplicationStopping.Register(OnStopping);
+            _hostApplicationLifetime.ApplicationStopped.Register(OnStopped);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+
+        private void OnStarted()
+        {
+            _logger.LogInformation(_configuration["PREFIX_MyKey"]);
+            _logger.LogInformation(_configuration["MyKey"]);
+
+            _logger.LogInformation("OnStarted...");
+        }
+
+        private void OnStopping()
+        {
+            _logger.LogInformation("OnStopping...");
+        }
+
+        private void OnStopped()
+        {
+            _logger.LogInformation("OnStopped...");
+        }
+    }
+}
--- FD.ShortUrl.Api/Program.cs
+++ FD.ShortUrl.Api/Program.cs
@@ -1,9 +1,13 @@
-using FD.ShortUrl.Api;
 
-await Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.UseStartup<Startup>();
-    })
-    .Build()
-    .RunAsync();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureHostOptions(o => o.ShutdownTimeout = TimeSpan.FromSeconds(5));
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "hello,world");
+
+app.MapGet("/stop", () => app.StopAsync());
+
+await app.RunAsync();
