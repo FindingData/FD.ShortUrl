8d2b71f2eb127b59ae6f9faf9b8fc9f50f933b4b
dragon.ice.995 <dragon.ice.995@hotmail.com>
2022-03-07 19:43:03 +0800

    fix

--- FD.ShortUrl.Api/Controllers/ConfigController.cs
+++ FD.ShortUrl.Api/Controllers/ConfigController.cs
@@ -41,7 +41,40 @@ namespace FD.ShortUrl.Api.Controllers
                            $"Name: {positionOptions.Name}");
         }
 
+        [HttpGet]
+        public IActionResult CommandLine()
+        {
+            return Content(
+                    $"Key1: '{_config["Key1"]}'\n" +
+                    $"Key2: '{_config["Key2"]}'\n" +
+                    $"Key3: '{_config["Key3"]}'\n" +
+                    $"Key4: '{_config["Key4"]}'\n" +
+                    $"Key5: '{_config["Key5"]}'\n" +
+                    $"Key6: '{_config["Key6"]}'");
+        }
 
+
+        [HttpGet]
+        public IActionResult Memory()
+        {
+            var myKeyValue = _config["MyKey"];
+            var title = _config["Position:Title"];
+            var name = _config["Position:Name"];
+            var defaultLogLevel = _config["Logging:LogLevel:Default"];
+
+
+            return Content($"MyKey value: {myKeyValue} \n" +
+                           $"Title: {title} \n" +
+                           $"Name: {name} \n" +
+                           $"Default Log Level: {defaultLogLevel}");
+        }
+
+        [HttpGet]
+        public IActionResult GetValue()
+        {
+            var number = _config.GetValue<int>("NumberKey", 99);
+            return Content($"{number}");
+        }
 
     }
 }
--- FD.ShortUrl.Api/Program.cs
+++ FD.ShortUrl.Api/Program.cs
@@ -1,18 +1,10 @@
-
-using FD.ShortUrl.Domain;
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureDefaults(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddConfig(builder.Configuration)
-    .Configure<PositionOption>(
-builder.Configuration.GetSection(PositionOption.PositionOptions));
-builder.Configuration.AddEnvironmentVariables();
-
-
 var app = builder.Build();
-Console.WriteLine($"EKey={Environment.GetEnvironmentVariable("EKey")}");
+
 app.MapControllers();
- 
 await app.RunAsync();
